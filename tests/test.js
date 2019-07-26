87fb775de68974bbd88130de08aca7c0ff34bb7b
Arshad Kazmi <arshadkazmi42@gmail.com>
2019-07-26 23:39:06 +0530

    Change forked repository to improve test

--- tests/test.js
+++ tests/test.js
@@ -2,7 +2,7 @@ const expect = require('chai').expect;
 
 const { Contributors, ParentRepo } = require('../index');
 
-const PARENT_REPO_URL = 'https://github.com/taskcluster/taskcluster';
+const PARENT_REPO_URL = 'https://github.com/mozilla-frontend-infra/react-lazylog';
 
 
 describe('validates gh contributors api response', () => {
@@ -14,7 +14,7 @@ describe('validates gh contributors api response', () => {
 
 describe('validates gh parent repo api response', () => {
   it('should return parent repo url', async () => {
-    const parentRepo = await ParentRepo('arshadkazmi42', 'taskcluster');
+    const parentRepo = await ParentRepo('arshadkazmi42', 'react-lazylog');
     expect(parentRepo).to.equal(PARENT_REPO_URL);
   });
 });
